fb5977d66079af85ae6b4fe08bf60e171e515d69
anton156 <57438883+anton156@users.noreply.github.com>
2021-04-24 15:53:13 +0200

    Add files via upload

---
+++ DZ3_SOAP_WSDL-02-master/DZ3_SOAP_WSDL-02/WebFormStudent.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormStudent.aspx.cs" Inherits="DZ3_SOAP_WSDL_02.WebFormStudent" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Upišite Id studenta</div>
+        <p>
+            <asp:TextBox ID="StudentNumber" runat="server"></asp:TextBox>
+            <asp:Button ID="StudentBtn" runat="server" OnClick="StudentBtn_Click" Text="Nadi podatke" />
+        </p>
+        <p>
+            <asp:Label ID="StudentInfo" runat="server"></asp:Label>
+            <asp:Label ID="StudentInfo2" runat="server"></asp:Label>
+        </p>
+        <asp:Label ID="StudentInfo3" runat="server"></asp:Label>
+        <p>
+            <asp:Label ID="StudentInfo4" runat="server"></asp:Label>
+        </p>
+        <asp:Label ID="StudentInfo5" runat="server"></asp:Label>
+        <p>
+            <asp:Label ID="StudentInfo6" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ DZ3_SOAP_WSDL-02-master/DZ3_SOAP_WSDL-02/WebService1.asmx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySqlConnector;
+using System.Data;
+
+namespace DZ3_SOAP_WSDL_02
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public int GetSquare(int a)
+        {
+            return (a * a);
+        }
+        [WebMethod]
+        public float konverzijaBAMToEUR(float bam)
+        {
+            return (float)(bam * 1.96);
+        }
+
+        [WebMethod]
+        public float konverzijaEURToBAM(float eur)
+        {
+            return (float)(eur * 0.51);
+        }
+    }
+}
